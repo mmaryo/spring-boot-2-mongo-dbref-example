d68da0c7ba2815306482d6b0544330926cc2343d
Mario <matarmario@gmail.com>
2017-09-20 18:58:11 +0200

    first commit

--- src/test/java/com/github/helios/SpringBoot2ExampleTest.java
+++ src/test/java/com/github/helios/SpringBoot2ExampleTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBoot2Example {
+public class SpringBoot2ExampleTest {
 
     @Autowired
     private ReactiveAccountRepository accountRepository;
